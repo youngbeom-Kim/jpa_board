7ad2e6db629d80f92c6276a1c5f02c06c6b2876c
youngbeom-Kim <abcd450196@gmail.com>
2023-03-07 17:17:22 +0900

    테스트 커밋

--- src/main/java/jpa/board/BoardApplication.java
+++ src/main/java/jpa/board/BoardApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BoardApplication {
 
-	//Test 커밋
-
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
 	}
