e15251293583b5be7332bdcab9659d6a22a369ad
youngbeom-Kim <abcd450196@gmail.com>
2023-06-23 16:30:22 +0900

    BoardController 생성

---
+++ src/main/java/jpa/board/controller/BoardController.java
@@ -0,0 +1,24 @@
+package jpa.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/")
+    public String list() {
+        return "board/list";
+    }
+
+    @GetMapping("/write")
+    public String write() {
+        return "board/write";
+    }
+
+    @GetMapping("/update")
+    public String upadte() {
+        return "board/update";
+    }
+
+}
