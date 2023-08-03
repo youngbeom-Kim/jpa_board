c99587bbbef9a017e8f0396bc6e2f78356508da4
youngbeom-Kim <abcd450196@gmail.com>
2023-08-03 18:35:47 +0900

    게시판 유효성 검사

--- src/main/java/jpa/board/controller/BoardController.java
+++ src/main/java/jpa/board/controller/BoardController.java
@@ -8,10 +8,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -40,12 +42,18 @@ public class BoardController {
 
     @GetMapping("/write")
     public String write(Model model) {
-        model.addAttribute("form", new BoardDto());
+        model.addAttribute("boardDto", new BoardDto());
         return "board/write";
     }
 
     @PostMapping("/write")
-    public String save(BoardDto boardDto) {
+    public String save(@Valid BoardDto boardDto, BindingResult result) {
+
+        //유효성검사 걸릴 시
+        if (result.hasErrors()) {
+            return "board/write";
+        }
+
         boardService.saveBoard(boardDto);
         return "redirect:/";
     }
